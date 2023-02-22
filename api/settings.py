4d2236f8ca1484bc13a52d745e320e119f681c29
lonebhen <ben.angmortey@gmail.com>
2023-02-22 21:57:42 +0000

    mysql setup

--- api/settings.py
+++ api/settings.py
@@ -76,11 +76,16 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
+import pymysql
+pymysql.install_as_MySQLdb()
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_ang',
+        'USER': 'root',
+        'PASSWORD': 'H@shn0de',
+        'PORT': 3306,
+        'HOST': '127.0.0.1'
     }
 }
 
